88123a3b223d4ef706efc5ae6a24ae7e7a19ca53
Dusty Diamond <julian@dustydiamond.de>
2022-02-02 21:27:05 +0100

    First Test

---
+++ FS22_greatDemand/greatDemands.lua
@@ -0,0 +1,21 @@
+--[[
+Great Demands Mod for FS22
+
+Author:   DustyDiamond
+Version:  0.0.1.0
+Modified: 2022-02-02
+
+twitch: https://www.twitch.tv/dustydiamond
+
+Changelog:
+v0.0.1:
+    First Draft for Script
+]]
+
+GreatDemands = Mod:init()
+
+GreatDemands:enableDebugMode()
+
+function GreatDemands:loadMap()
+    print("Great Demands Mod Loaded!")
+end
