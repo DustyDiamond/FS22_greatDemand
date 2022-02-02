df907f7e7c639b871300dc80052b10bb2857c005
Dusty Diamond <julian@dustydiamond.de>
2022-02-02 21:31:17 +0100

    v0.0.2

--- FS22_greatDemand/greatDemands.lua
+++ FS22_greatDemand/greatDemands.lua
@@ -2,13 +2,13 @@
 Great Demands Mod for FS22
 
 Author:   DustyDiamond
-Version:  0.0.1.0
+Version:  0.0.2.0
 Modified: 2022-02-02
 
 twitch: https://www.twitch.tv/dustydiamond
 
 Changelog:
-v0.0.1:
+v0.0.2:
     First Draft for Script
 ]]
 
